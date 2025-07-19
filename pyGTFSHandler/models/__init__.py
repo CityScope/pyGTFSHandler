6279e9f2a8ccbd5b5f638db91ae204fecea2549d
MiguelUrenaPliego <miguel.urena.pliego@gmail.com>
2025-07-19 20:28:54 +0200

    Add Routes to __inin__

--- pyGTFSHandler/models/__init__.py
+++ pyGTFSHandler/models/__init__.py
@@ -2,10 +2,12 @@ from .stop_times import StopTimes
 from .stops import Stops
 from .trips import Trips
 from .calendar import Calendar
+from .routes import Routes
 
 __all__ = [
     StopTimes.__name__,
     Stops.__name__,
     Trips.__name__,
     Calendar.__name__,
+    Routes.__name__,
 ]
